dd4a3ba9317260ac5b8a1c35174cc3dcac878103
HIMANSHU GOYAL <51674979+himanshucsengineer@users.noreply.github.com>
2020-06-14 17:46:01 +0530

    index ui changes

--- footer.php
+++ footer.php
@@ -1,3 +1,21 @@
+  <style>
+    .foot{
+      width: 100%;
+      height: auto;
+      background-color: rgba(0, 0, 0, 0.85);
+      padding-top: 1rem;
+      padding-bottom: .5rem;
+    }
+    </style>
+  
+  <footer class="foot" >
+      <div class="container text-center">
+        <p style="color: grey; font-size:13px;">@2020 ENIACODER ALL RIGHTS RESERVED. DEVELOPED BY <span ><a style="color: #f05b23; text-decoration:none;" href="http://eniacoder.com/" target="_blank">ENIACODER</a></span></p>
+      </div>
+  </footer>
+  
+  
+  
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
     <link rel="stylesheet" href="styles.css">
     <link href="https://fonts.googleapis.com/css2?family=Knewave&display=swap" rel="stylesheet">
@@ -5,3 +23,5 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    </body>
+</html>  
